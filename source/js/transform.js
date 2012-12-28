c491890a69352da234a8bf1f303d059bb0e820bd
Blaise Kal <blaisekal@gmail.com>
2012-12-28 22:08:36 +0100

    Speed, code and visual improvements in anti-aliasing

--- source/js/transform.js
+++ source/js/transform.js
@@ -83,113 +83,98 @@ Transform.prototype = {
     },
 
     /**
-     * @param {XSS.ShapePixels} origPixels
-     * @param {XSS.ShapePixels} zoomedPixels
+     * @param {XSS.ShapePixels} x1Pixels
+     * @param {XSS.ShapePixels} x2Pixels
      * @param {number=} shiftX
      * @param {number=} shiftY
      * @return {XSS.ShapePixels}
      * @private
      */
-    _antiAlias: function(origPixels, zoomedPixels, shiftX, shiftY) {
-        var has, push;
+    _antiAlias: function(x1Pixels, x2Pixels, shiftX, shiftY) {
+        var index, has, push, cache = [];
 
         has = function(p, x, y) {
-            return this._hasPixel(origPixels, p[0] + x, p[1] + y);
+            x = p[0] + x;
+            y = p[1] + y;
+            index = x + y * XSS.PIXELS_V;
+            if (typeof cache[index] === 'undefined') {
+                cache[index] = this._hasPixel(x1Pixels, x, y);
+            }
+            return cache[index];
         }.bind(this);
 
         push = function(p, x, y) {
             x = p[0] * 2 + x + shiftX;
             y = p[1] * 2 + y + shiftY;
-            if (!this._hasPixel(zoomedPixels, x, y)) {
-                zoomedPixels.push([x, y]);
+            if (!this._hasPixel(x2Pixels, x, y)) {
+                x2Pixels.push([x, y]);
             }
         }.bind(this);
 
-        for (var i = 0, m = origPixels.length; i < m; i++) {
-            var p = origPixels[i];
+        // Walk through pixels, see if neighbour pixels match a pattern where
+        // pixels are visually missing, and fill those gaps.
+        for (var i = 0, m = x1Pixels.length; i < m; i++) {
+            var xx, yy, px, py, p = x1Pixels[i];
 
-            // ?
-            // /#
-            // x/?
+            // Vertical bottom-left to top-right fill.
+            // Mainly for 2x zoom.
             if (has(p, 1, -1) &&
                 !has(p, 0, -1) &&
-                !has(p, 1, 0) &&
-                (!has(p, 2, 0) || !has(p, 0, -2))
-            ) {
-                push(p, +2, +0); // Right
-                push(p, +1, -1); // Top
-            }
-
-            // x\?
-            // \#
-            // ?
-            if (has(p, 1, 1) &&
-                !has(p, 1, 0) &&
-                !has(p, 0, 1) &&
-                (!has(p, 2, 0) || !has(p, 0, 2))
-            ) {
-                push(p, +2, +1); // Right
-                push(p, +1, +2); // Bottom
-            }
-
-            // x\
-            // ##\
-            // # #
-            else if (has(p, 0, 1) &&
-                has(p, 1, 1) &&
-                has(p, 0, 2) &&
-                has(p, 0, 2) &&
-                has(p, 2, 2) &&
-                !has(p, 1, 0) &&
-                !has(p, 2, 1)
-            ) {
-                push(p, +2, +1);
-                push(p, +4, +3);
-            }
-
-            //  /x
-            // /##
-            // # #
-            else if (has(p, 0, 1) &&
-                has(p, -1, 1) &&
-                has(p, 0, 2) &&
-                has(p, -2, 2) &&
-                !has(p, -1, 0) &&
-                !has(p, -2, 1)
-            ) {
-                push(p, -1, +1);
-                push(p, -3, +3);
-            }
-
-            // # #
-            // \##
-            //  \x
-            else if (has(p, -2, -2) &&
-                has(p, 0, -2) &&
-                has(p, -1, -1) &&
-                has(p, 0, -1) &&
-                !has(p, -2, -1) &&
-                !has(p, -1, 0)
-            ) {
-                push(p, -3, -2);
-                push(p, -1, 0);
-            }
-
-            // # #
-            // ##/
-            // x/
-            else if (has(p, 2, -2) &&
-                has(p, 0, -2) &&
-                has(p, 1, -1) &&
-                has(p, 0, -1) &&
-                !has(p, 2, -1) &&
                 !has(p, 1, 0)
             ) {
-                push(p, 4, -2);
-                push(p, 2, 0);
+                push(p, +2, +0);
+                push(p, +1, -1);
             }
+
+            // Vertical top-left to bottom-right fill.
+            // Mainly for 2x zoom.
+            if (has(p, 1, 1) &&
+                !has(p, 1, 0) &&
+                !has(p, 0, 1)
+            ) {
+                push(p, +2, +1);
+                push(p, +1, +2);
+            }
+
+            // Vertical bottom-left and top-right fill.
+            // 4x zoom, 4 directions.
+            for (xx = -1; xx <= 1; xx += 2) {
+                for (yy = -1; yy <= 1; yy += 2) {
+                    if (has(p, xx, yy) &&
+                        has(p, xx * 2, yy * 2) &&
+                        !has(p, xx, 0) &&
+                        !has(p, xx * 2, yy)
+                    ) {
+                        px = (xx < 0) ? -1 : 2;
+                        py = (yy < 0) ? 0 : 1;
+                        push(p, px, py);
+
+                        px = (xx < 0) ? -3 : 4;
+                        py = (yy < 0) ? -2 : 3;
+                        push(p, px, py);
+                    }
+                }
+            }
+
+            // Misc square corners fill.
+            // 4x zoom, 4 directions.
+            for (xx = -1; xx <= 1; xx += 2) {
+                for (yy = -1; yy <= 1; yy += 2) {
+                    if (has(p, -xx, yy) &&
+                        has(p, xx, yy) &&
+                        has(p, xx * -2, yy * 3) &&
+                        !has(p, xx * -2, yy * 2)
+                    ) {
+                        px = (xx < 0) ? 4 : -3;
+                        py = (yy < 0) ? -1 : 2;
+                        push(p, px, py);
+                    }
+                }
+            }
+
         }
-        return zoomedPixels;
+
+        return x2Pixels;
     },
 
     /**
