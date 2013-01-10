3c42473ef8bec46dcb9c99f9c75ede857977b1ff
Blaise Kal <blaise@layar.com>
2013-01-10 14:21:51 +0100

    Cache references in antialias transform

--- source/js/transform.js
+++ source/js/transform.js
@@ -91,7 +91,7 @@ Transform.prototype = {
      * @private
      */
     _antiAlias: function(x1Pixels, x2Pixels, shiftX, shiftY) {
-        var index, cache = [];
+        var index, cache = [], has = XSS.transform.has;
 
         /**
          * @param {Array.<number>} p
@@ -99,12 +99,12 @@ Transform.prototype = {
          * @param {number} y
          * @return {boolean}
          */
-        function has(p, x, y) {
+        function hasProxy(p, x, y) {
             x = p[0] + x;
             y = p[1] + y;
             index = x + y * XSS.PIXELS_V;
             if (typeof cache[index] === 'undefined') {
-                cache[index] = XSS.transform.has(x1Pixels, x, y);
+                cache[index] = has(x1Pixels, x, y);
             }
             return cache[index];
         }
@@ -118,7 +118,7 @@ Transform.prototype = {
         function push(p, x, y) {
             x = p[0] * 2 + x + shiftX;
             y = p[1] * 2 + y + shiftY;
-            if (!XSS.transform.has(x2Pixels, x, y)) {
+            if (!has(x2Pixels, x, y)) {
                 x2Pixels.push([x, y]);
             }
         }
@@ -130,9 +130,9 @@ Transform.prototype = {
 
             // Vertical bottom-left to top-right fill.
             // Mainly for 2x zoom.
-            if (has(p, 1, -1) &&
-                !has(p, 0, -1) &&
-                !has(p, 1, 0)
+            if (hasProxy(p, 1, -1) &&
+                !hasProxy(p, 0, -1) &&
+                !hasProxy(p, 1, 0)
             ) {
                 push(p, +2, +0);
                 push(p, +1, -1);
@@ -140,9 +140,9 @@ Transform.prototype = {
 
             // Vertical top-left to bottom-right fill.
             // Mainly for 2x zoom.
-            if (has(p, 1, 1) &&
-                !has(p, 1, 0) &&
-                !has(p, 0, 1)
+            if (hasProxy(p, 1, 1) &&
+                !hasProxy(p, 1, 0) &&
+                !hasProxy(p, 0, 1)
             ) {
                 push(p, +2, +1);
                 push(p, +1, +2);
@@ -152,10 +152,10 @@ Transform.prototype = {
             // 4x zoom, 4 directions.
             for (xx = -1; xx <= 1; xx += 2) {
                 for (yy = -1; yy <= 1; yy += 2) {
-                    if (has(p, xx, yy) &&
-                        has(p, xx * 2, yy * 2) &&
-                        !has(p, xx, 0) &&
-                        !has(p, xx * 2, yy)
+                    if (hasProxy(p, xx, yy) &&
+                        hasProxy(p, xx * 2, yy * 2) &&
+                        !hasProxy(p, xx, 0) &&
+                        !hasProxy(p, xx * 2, yy)
                     ) {
                         px = (xx < 0) ? -1 : 2;
                         py = (yy < 0) ? 0 : 1;
@@ -172,10 +172,10 @@ Transform.prototype = {
             // 4x zoom, 4 directions.
             for (xx = -1; xx <= 1; xx += 2) {
                 for (yy = -1; yy <= 1; yy += 2) {
-                    if (has(p, -xx, yy) &&
-                        has(p, xx, yy) &&
-                        has(p, xx * -2, yy * 3) &&
-                        !has(p, xx * -2, yy * 2)
+                    if (hasProxy(p, -xx, yy) &&
+                        hasProxy(p, xx, yy) &&
+                        hasProxy(p, xx * -2, yy * 3) &&
+                        !hasProxy(p, xx * -2, yy * 2)
                     ) {
                         px = (xx < 0) ? 4 : -3;
                         py = (yy < 0) ? -1 : 2;
