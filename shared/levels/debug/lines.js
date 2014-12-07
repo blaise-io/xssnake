095df647f964f7da812dc857c45a9ae22d95f64d
Blaise Kal <blaisekal@gmail.com>
2014-12-07 15:28:31 +0100

    Remove gravity

--- shared/levels/debug/lines.js
+++ shared/levels/debug/lines.js
@@ -6,7 +6,6 @@
  * @constructor
  */
 xss.levels.LinesLevel = function(config) {
-    config.gravity = [-5, 0];
     config.level = xss.data.levels.lines;
     xss.level.Level.call(this, config);
 };
