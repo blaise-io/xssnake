b12577c3f09cd7c8ebf76295c17a97e23b3609b2
Blaise Kal <blaisekal@gmail.com>
2021-04-06 22:46:35 +0200

    Use regular es6 imports

--- server/netcode/server.ts
+++ server/netcode/server.ts
@@ -1,26 +1,22 @@
-import * as events from "events";
-import * as ws from "ws";
+import { EventEmitter } from "events";
+import { Server as WebsocketServer } from "ws";
 import { SERVER_HOST, SERVER_PATH, SERVER_PORT } from "../../shared/config";
 import { ServerRoomManager } from "../room/roomManager";
 import { ServerPlayer } from "../room/serverPlayer";
 
-const WebSocket = require("ws");
-const EventEmitter = require("events").EventEmitter;
-
 export class Server {
-    private emitter: events.EventEmitter;
+    private emitter: EventEmitter;
     private roomManager?: ServerRoomManager;
-    private ws: ws.Server;
+    private ws: WebsocketServer;
 
     constructor() {
         this.emitter = new EventEmitter();
         this.roomManager = new ServerRoomManager(this);
-        this.ws = new WebSocket.Server({
+        this.ws = new WebsocketServer({
             host: SERVER_HOST,
             port: SERVER_PORT,
             path: SERVER_PATH,
         });
-
         this.ws.on("connection", () => {
             new ServerPlayer(this, this.ws);
         });
